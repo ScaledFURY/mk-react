ca4e77568673f55e25531255d94591844327456b
Stephen Blackstone <sblackstone@gmail.com>
2020-06-12 19:18:05 -0400

    Add api_client, config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  apiEndpoint: "api.burnerdomain.xyz",
+  checkoutDefaultVariantId: "32453520654473",
+  defaultShippingZone: "US",
+  nextUrl: 'http://localhost:3003/upsell1.html',  
+}
+
+export default config;

365c7eb26bd4cefe9f9f20da0602689b83225f15
Stephen Blackstone <sblackstone@gmail.com>
2020-06-14 20:37:31 -0400

    Fixing case issue

--- src/blarg.js
+++ src/blarg.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './app.css';
 import ApiClient from './api_client';
 import loadSettings from './load_settings';
-import SubtotalValue from './subtotal_value';
+import SubtotalValue from './components/subtotal_value';
 import Loading from './loading';
 import Prism from 'prismjs';
 import PrismJsx from 'prismjs/components/prism-jsx.min';

b922924afc35aefeff7cc56acb10b65559c99300
Stephen Blackstone <sblackstone@gmail.com>
2020-07-10 09:07:50 -0400

    Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import Hello from './Hello';
+import Lander1 from './Lander1';
+import Lander2 from './Lander2';
+import Checkout from './checkout'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <Hello name="felipe" />
+    <Router>
+      <Switch>
+        <Route path="/lander1">
+          <Lander1 />
+        </Route>
+        <Route path="/lander2">
+          <Lander2 />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Lander1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function(props) {
+  return (
+    <h1>Lander 1</h1>
+  )
+}

b57680322f225fa0da4480247b00315851c926b4
Stephen Blackstone <sblackstone@gmail.com>
2020-08-17 09:13:08 -0400

    Get demo ready

--- src/Demo.js
+++ src/Demo.js
@@ -1,14 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import Prism from "prismjs";
+import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-coy.css';
+import './Demo.scss';
 
 
 export default function(props) {
   window.stuff = props;
 
+  const codeStr = '<Stuff />';
+
+  useEffect(()=> {
+    Prism.highlightAll();
+  });
+
   return (
     <React.Fragment>
       <h1>Demo</h1>
       <div className="demo-container">
-        {JSON.stringify(props.cart)}
+        <div className="left">
+          <pre>
+            <code className="language-html">
+            {`
+              <Stuff />
+            `}
+            </code>
+          </pre>
+        </div>
+    
+        <div className="right">
+            aaaa
+        </div>
+    
       </div>
 
     </React.Fragment>
